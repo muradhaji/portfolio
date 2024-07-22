02ee9a9b2681ce3aa72cc95aa65e0d8d38dc1864
muradhaji <muradhajiyev42@icloud.com>
2024-07-22 23:57:08 +0400

    feat: SocialIcon component created

---
+++ src/components/SocialIcon.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import { ReactComponent as LinkedInSvg } from '../images/icons/linkedin.svg';
+import { ReactComponent as WhatsAppSvg } from '../images/icons/whatsapp.svg';
+import { ReactComponent as TelegramSvg } from '../images/icons/telegram.svg';
+import { ReactComponent as EnvelopeSvg } from '../images/icons/envelope.svg';
+import { ReactComponent as GitHubSvg } from '../images/icons/github.svg';
+import { ReactComponent as LeetCodeSvg } from '../images/icons/leetcode.svg';
+import { ReactComponent as XMarkSvg } from '../images/icons/xmark.svg';
+
+const mediaEnum = {
+  linkedin: <LinkedInSvg />,
+  whatsapp: <WhatsAppSvg />,
+  telegram: <TelegramSvg />,
+  email: <EnvelopeSvg />,
+  github: <GitHubSvg />,
+  leetcode: <LeetCodeSvg />,
+};
+
+function SocialIcon({ media = null, type = null, ...props }) {
+  return (
+    <a className={`socialIcon ${type ? type : ''}`} {...props}>
+      {media ? mediaEnum[media] : <XMarkSvg />}
+    </a>
+  );
+}
+
+export default SocialIcon;
+
+SocialIcon.propTypes = {
+  media: PropTypes.oneOf([
+    'linkedin',
+    'whatsapp',
+    'telegram',
+    'email',
+    'github',
+    'leetcode',
+  ]),
+  type: PropTypes.oneOf(['transparent', 'stroke']),
+};
