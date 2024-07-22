744115d91af46c5f34431660ba358a2bab0a70f1
muradhaji <muradhajiyev42@icloud.com>
2024-07-22 22:46:38 +0400

    feat: Button component created

---
+++ src/components/Button.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+
+function Button({
+  text = 'Button',
+  type = 'button',
+  to = '',
+  appearance = 'solid',
+  onClick = () => {},
+  icon = null,
+}) {
+  return type === 'link' ? (
+    <Link to={to} className={`button ${appearance} ${icon ? 'hasIcon' : ''}`}>
+      <span className='text'>{text}</span>
+      {icon && <span className='icon'>{icon}</span>}
+    </Link>
+  ) : type === 'hashlink' ? (
+    <HashLink
+      to={to}
+      className={`button ${appearance} ${icon ? 'hasIcon' : ''}`}
+    >
+      <span className='text'>{text}</span>
+      {icon && <span className='icon'>{icon}</span>}
+    </HashLink>
+  ) : (
+    <div
+      onClick={onClick}
+      className={`button ${appearance} ${icon ? 'hasIcon' : ''}`}
+    >
+      <span className='text'>{text}</span>
+      {icon && <span className='icon'>{icon}</span>}
+    </div>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  type: PropTypes.oneOf(['link', 'hashlink', 'button']),
+  to: PropTypes.string,
+  appearance: PropTypes.oneOf(['solid', 'stroke']),
+  onClick: PropTypes.func,
+  icon: PropTypes.node,
+};
+
+export default Button;
